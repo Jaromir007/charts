234793def90ef392555ae355d94d33d62ca4a723
Jaromir007 <jaromir.obitko@student.gyarab.cz>
2025-05-19 09:17:26 +0200

    CORS works

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user_list
+from .views import user_list, hello_api
 
 urlpatterns = [
     path('users/', user_list, name='user-list'),
+    path('hello/', hello_api, name='hello-api')
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 from rest_framework.decorators import api_view
@@ -11,3 +12,6 @@ def user_list(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
+
+def hello_api(reuest):
+    return JsonResponse({'message' : 'Hello, API!'})
